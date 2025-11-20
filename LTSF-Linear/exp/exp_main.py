8b5dcfed79240b529eda052351e105fd8d33796d
zhangtina0103 <awsometinazhang@gmail.com>
2025-11-19 21:53:31 -0500

    edited freq hybrid

--- LTSF-Linear/exp/exp_main.py
+++ LTSF-Linear/exp/exp_main.py
@@ -1,7 +1,6 @@
 from data_provider.data_factory import data_provider
 from exp.exp_basic import Exp_Basic
-from models import Informer, Autoformer, Transformer, DLinear, Linear, NLinear
-from models.FreqHybrid import FreqHybrid
+from models import Informer, Autoformer, Transformer, DLinear, Linear, NLinear, FreqHybrid
 from utils.tools import EarlyStopping, adjust_learning_rate, visual, test_params_flop
 from utils.metrics import metric
 
